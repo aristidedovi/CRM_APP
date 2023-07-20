1d2f2be0295fac8880c29929e44f944dfd4396a2
aristide <mac@macs-MacBook-Pro.local>
2023-07-20 10:05:59 +0000

    update front service file

--- front-React/src/services/apiService.js
+++ front-React/src/services/apiService.js
@@ -1,19 +1,20 @@
  // Remplacez par l'URL de base de votre API
+ var apiUrl = 'http://localhost:5000/api/v1';
+
 
 var options = {
     headers: {
       'Authorization': `Bearer ${sessionStorage.getItem('Auth Token')}`,
     }
   }
-var apiUrl = '';
 
-  if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-        // dev code
-        apiUrl = 'http://localhost:5000/api/v1';
-    } else {
-        // dev code
-        apiUrl = 'http://localhost:5000/api/v1';
-    }
+  // if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  //       // dev code
+  //       apiUrl = 'http://localhost:5000/api/v1';
+  //   } else {
+  //       // dev code
+  //       apiUrl = 'http://localhost:5000/api/v1';
+  //   }
 
 export const apiService = {
   get: (endpoint) => {
