42a7160c43f86cb8bf0f05bd3989d56e6ed8a499
aristide <mac@macs-MacBook-Pro.local>
2023-07-24 15:57:15 +0000

    update service api url

--- front-React/src/services/apiService.js
+++ front-React/src/services/apiService.js
@@ -13,7 +13,8 @@ const options = {
     apiUrl = 'http://localhost:5000/api/v1';
   } else if(!process.env.NODE_ENV || process.env.NODE_ENV === 'production') {
     // prod code
-    apiUrl = 'http://144.126.232.79:5000/api/v1';
+    const currentHost = `${window.location.protocol}//${window.location.hostname}`;
+    apiUrl = currentHost+':5000/api/v1';
   }else {
     // test code
     apiUrl = 'http://localhost:5000/api/v1';
