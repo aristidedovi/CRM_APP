21999f9186e275861bf4a4d8ec7b0adf306d2da6
aristide <mac@macs-MacBook-Pro.local>
2023-08-02 14:30:00 +0000

    add caching system for front

--- backend-Flask/flaskr/api1/routes.py
+++ backend-Flask/flaskr/api1/routes.py
@@ -64,7 +64,6 @@ def after_request(response):
     #                     '*')
     
     response.headers["Access-Control-Allow-Origin"] = "*"
-    
     response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
     response.headers["Access-Control-Allow-Headers"] = "Content-Type, X-Requested-With, Authorization,true"
     response.headers['Content-Type'] = 'application/json'
