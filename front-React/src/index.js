d4807fb8bfea158449cfb03a7cf10674fb866c1a
aristide <mac@macs-MacBook-Pro.local>
2023-08-08 15:14:10 +0000

    add spinner on front

--- front-React/src/index.js
+++ front-React/src/index.js
@@ -22,10 +22,14 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import "assets/plugins/nucleo/css/nucleo.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "assets/scss/argon-dashboard-react.scss";
+import "react-progress-2/main.css";
+
 
 import AdminLayout from "layouts/Admin.js";
 import AuthLayout from "layouts/Auth.js";
 import { ToastContainer } from "react-toastify";
+import Progress from "react-progress-2";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -37,5 +41,9 @@ root.render(
       <Route path="*" element={<Navigate to="/admin/index" replace />} />
     </Routes>
     <ToastContainer />
+    <Progress.Component
+    style={{background: 'green'}}
+    thumbStyle={{background: 'red'}}
+    />
   </BrowserRouter>
 );
