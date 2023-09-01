b221d3d28bbf7d3d524ec66cc1693a3038a4ee2f
aristide <mac@macs-MacBook-Pro.local>
2023-09-01 11:28:18 +0000

    init

--- backend-Flask/flaskr/front/__init__.py
+++
@@ -1,9 +0,0 @@
-# API Blueprint
-from flask import Blueprint
-from flask_cors import CORS
-
-# initializing the blueprint
-front = Blueprint('front', __name__)
-CORS(front, resources={r'/*': {'origins': '*'}})
-# importing routes
-from . import routes
